f8048c302b17c7f6cb0cac6c207b2fb7458307d2
Ibad laskar <ibalaskar6@gmail.com>
2023-08-12 19:39:34 +0530

    odd.java

---
+++ odd.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class odd {
+    
+public static void main(String[] args) {
+ 
+    
+Scanner sc=new Scanner(System.in);
+
+System.out.println("Enter a number");
+int a=sc.nextInt();
+
+
+if(a%2==0){
+    System.out.print("The Given Number is even");
+
+
+}
+
+else
+System.out.print("The Given number is odd");
+sc.close();
+}
+}
